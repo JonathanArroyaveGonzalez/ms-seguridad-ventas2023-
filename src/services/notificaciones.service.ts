b04a8de52cc3f4c3b5f0736108e421d67c14bff4
Jonathan Arroyave <arroyave3000g@gmail.com>
2023-04-04 20:21:43 -0500

    Avance video 28

---
+++ src/services/notificaciones.service.ts
@@ -0,0 +1,21 @@
+import {injectable, /* inject, */ BindingScope} from '@loopback/core';
+const fetch = require("node-fetch");
+
+
+
+@injectable({scope: BindingScope.TRANSIENT})
+export class NotificacionesService {
+  constructor(/* Add @inject to inject parameters */) {}
+
+  /*
+   * Add service methods here
+   */
+
+  EnviarCorreoElectronico(datos: any, url: string){
+    fetch(url,{
+      method: 'post',
+      body: JSON.stringify(datos),
+      headers: {'Content-Type': 'application/json'},
+    })
+  }
+}
